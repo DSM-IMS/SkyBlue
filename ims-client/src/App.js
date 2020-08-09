8e3932c490fd396673db502c4862a95faf7cad67
CutyApple <dbtldhs0915@naver.com>
2020-08-09 14:24:58 +0900

    [CREATE] 파일/디렉토리 init

--- ims-client/src/App.js
+++ ims-client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import * as S from './components/styles';
 
 const App = () => {
-	return <div></div>;
+	return (
+		<S.Container>
+			<S.GlobalStyle />
+		</S.Container>
+	);
 };
 
 export default App;
---
+++ ims-client/src/components/styles.js
@@ -0,0 +1,15 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	html, body {
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+`;
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	background: black;
+`;

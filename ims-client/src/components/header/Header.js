adecb7953ebfeadb93f81a8e84b56083f007e1c2
CutyApple <dbtldhs0915@naver.com>
2020-08-09 15:48:28 +0900

    [ADD] <Header.js> Header Button UI 제작

--- ims-client/src/components/header/Header.js
+++ ims-client/src/components/header/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import * as S from './styles';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<S.Container>
 			<S.Section>IMS</S.Section>
+			<S.ButtonWrap>
+				<S.HeaderButton as={Link} to="/">
+					개발자 소개
+				</S.HeaderButton>
+				<S.HeaderButton as={Link} to="/">
+					로그아웃
+				</S.HeaderButton>
+			</S.ButtonWrap>
 		</S.Container>
 	);
 };
--- ims-client/src/components/header/styles.js
+++ ims-client/src/components/header/styles.js
@@ -8,14 +8,33 @@ export const Container = styled.div`
 	background: #f2f2f2;
 	align-items: center;
 	justify-content: center;
+	box-sizing: border-box;
 `;
 
 export const Section = styled.div`
-	width: 50%;
+	width: 40%;
 	height: 60px;
 	color: white;
 	background: #a6a6a6;
 	text-align: center;
 	font-size: 32px;
 	line-height: 60px;
+`;
+
+export const ButtonWrap = styled.div`
+	position: absolute;
+	float: right;
+	top: 46px;
+	right: 20px;
+`;
+
+export const HeaderButton = styled.input`
+	font-size: 12px;
+	color: black;
+	text-decoration: none;
+	margin-right: 10px;
+
+	&:hover {
+		font-weight: bold;
+	}
 `;

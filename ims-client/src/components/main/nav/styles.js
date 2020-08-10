c763c75b191b9a807301b0c7a4983567e2d9a4a2
CutyApple <dbtldhs0915@naver.com>
2020-08-10 09:51:12 +0900

    [MODIFY] <Nav.js> Nav 스타일 조정

--- ims-client/src/components/main/nav/styles.js
+++ ims-client/src/components/main/nav/styles.js
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
-	width: 200px;
+	min-width: 200px;
 	height: 100%;
 	border-right: 1px solid gray;
 	flex-direction: column;
+	box-sizing: border-box;
 `;
 
 export const NavFirstItem = styled.div`

a0201a495dd4609a1696e116eb74ba050f539021
CutyApple <dbtldhs0915@naver.com>
2020-08-11 11:24:33 +0900

    [CREATE] <manageForm.js> manageForm init

--- ims-client/src/components/main/manageForm/manageForm.js
+++ ims-client/src/components/main/manageForm/manageForm.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import * as S from './styles';
 
 const manageForm = () => {
-	return <div></div>;
+	return <S.Container></S.Container>;
 };
 
 export default manageForm;

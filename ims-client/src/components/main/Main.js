b6b255ae2a099a79826fa6e55019dec1f586b461
CutyApple <dbtldhs0915@naver.com>
2020-08-11 14:22:38 +0900

    [ADD] <manageForm.js> manageForm Nav UI

--- ims-client/src/components/main/Main.js
+++ ims-client/src/components/main/Main.js
@@ -3,12 +3,14 @@ import * as S from './styles';
 import { Route } from 'react-router-dom';
 import Nav from './nav/Nav';
 import Form from './form/Form';
+import manageForm from './manageForm/manageForm';
 
 const Main = () => {
 	return (
 		<S.Container>
 			<Nav />
 			<Route exact path="/main" component={Form} />
+			<Route path="/main/form" component={manageForm} />
 		</S.Container>
 	);
 };

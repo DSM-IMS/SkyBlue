132082d29a0e19d098440b4e3b70f166208b3f59
CutyApple <dbtldhs0915@naver.com>
2020-08-11 14:22:38 +0900

    [ADD] <manageForm.js> manageForm Nav UI

--- ims-client/src/components/main/manageForm/manageForm.js
+++ ims-client/src/components/main/manageForm/manageForm.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import * as S from './styles';
+import { NavLink } from 'react-router-dom';
 
 const manageForm = () => {
-	return <S.Container></S.Container>;
+	return (
+		<S.Container>
+			<S.SelectNav>
+				<S.SelectNavItem
+					as={NavLink}
+					activeStyle={S.SelectNavItemActive}
+					to="/main/form/"
+				>
+					폼 수정
+				</S.SelectNavItem>
+				<S.SelectNavItem
+					as={NavLink}
+					activeStyle={S.SelectNavItemActive}
+					to="/main/form/result"
+				>
+					결과 통계 보기
+				</S.SelectNavItem>
+			</S.SelectNav>
+		</S.Container>
+	);
 };
 
 export default manageForm;
--- ims-client/src/components/main/manageForm/styles.js
+++ ims-client/src/components/main/manageForm/styles.js
@@ -2,4 +2,39 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
 	display: flex;
+	width: 100%;
+	height: 100%;
+	background: white;
+	flex-direction: column;
 `;
+
+export const SelectNav = styled.div`
+	display: flex;
+	width: 100%;
+	height: 60px;
+	background: rgba(0, 0, 0, 0);
+	align-items: flex-start;
+`;
+
+export const SelectNavItem = styled.a`
+	width: 140px;
+	height: 100%;
+	border-right: 1px solid gray;
+	background: #f2f2f2;
+	color: black;
+	font-size: 18px;
+	text-align: center;
+	line-height: 60px;
+	text-decoration: none;
+
+	transition: all 0.3s ease-in-out;
+
+	&:hover {
+		width: 160px;
+	}
+`;
+
+export const SelectNavItemActive = {
+	borderBottom: '1px solid #f2f2f2',
+	fontWeight: 'bold',
+};
